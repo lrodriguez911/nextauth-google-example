349bebd31359dacb0250f2c7cbcfcf3e54e2573f
Lucas Rodriguez <lrodriguez.arevalosc@gmail.com>
2023-09-23 22:13:15 -0300

    add provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,12 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import Navbar from '@/components/Navbar'
+import Providers from './Providers'
+
+
+
+
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,7 +22,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+      <Providers>
+
+        <Navbar/>
+        {children}</body>
+      </Providers>
     </html>
   )
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,27 @@
-import React from 'react'
+"use client"
+
+import Link from 'next/link'
+import {signIn, useSession} from "next-auth/react";
 
 function Navbar() {
+
+  const {data: session} = useSession();
+
   return (
-    <div>Navbar</div>
+    <nav className='bg-slate-900 flex items-center  justify-between px-24 py-3 text-white'>
+        <Link href="/">
+        <h1>NextGoogle</h1>
+        </Link>
+
+        <div className='flex gap-x-2 items-center'>
+            <Link href="/dashboard">
+            Dashboard
+            </Link>
+            <button onClick={() => signIn()} className='bg-sky-400 px-3 py-2 rounded'>
+                Sign In
+            </button>
+        </div>
+    </nav>
   )
 }
 
