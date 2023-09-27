b82ea1afe445bb05946c9a30846c180cc22c024d
Lucas Rodriguez <lrodriguez.arevalosc@gmail.com>
2023-09-26 22:59:28 -0300

    add callback url

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,13 +2,14 @@
 
 import Link from 'next/link'
 import {signIn, useSession, signOut} from "next-auth/react";
+
 import Image from 'next/image'
 
 // Navbar
 function Navbar() {
 
   const {data: session} = useSession();
-
+ 
   return (
     <nav className='bg-slate-900 flex items-center  justify-between px-24 py-3 text-white'>
         <Link href="/">
@@ -21,7 +22,12 @@ function Navbar() {
             </Link>
             <p>{session.user.name} {session.user.email}</p>
             <Image src={session.user.image}  width="100" height="100" alt="user image" />
-            <button onClick={() => signOut()} className='bg-sky-400 px-3 py-2 rounded'>
+            <button onClick={async () => {
+              await signOut({
+                callbackUrl: '/'
+              }) 
+              }
+              } className='bg-sky-400 px-3 py-2 rounded'>
                 Sign Out
             </button>
         </div>) : ( <button onClick={() => signIn()} className='bg-sky-400 px-3 py-2 rounded'>
